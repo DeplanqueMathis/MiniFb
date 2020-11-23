b6a91ea53f9ebe742698b05d89a851d44f77986e
Francesca <francesca.fdv28@gmail.com>
2020-11-23 10:28:40 +0100

    remplacement du absolute par un display grid

--- index.php
+++ index.php
@@ -44,7 +44,7 @@ if (isset($_SESSION['info'])) {
             <img class='img_avatar' src='images/avatars/"  . $_SESSION['avatar'] . "'> 
 			<li class='bonjour'>Bonjour " . $_SESSION['login'] . " !</li>
             <li class='deconnexion'> <a href='index.php?action=deconnexion'>Déconnexion</a></li>";
-            echo "<li class='vos_amis' ><a href='index.php?action=amis'>Tous vos amis sont ici </a><img class='amis_fleche' src='images/fleche.png' alt='fleche'></li>";
+            echo "<div id='mon_mur' ><li class='vos_amis' ><a href='index.php?action=amis'>Tous vos amis sont ici </a><img class='amis_fleche' src='images/fleche.png' alt='fleche'></li>";
             echo "<li class='invit'><a href='index.php?action=send'>Vos invitations envoyées</a> </li>";
             echo "<li class='invit_recues'><a href='index.php?action=recep'>Vos invitations reçues</a></li>";
 			echo "<div class='mes_posts'>
@@ -54,7 +54,7 @@ if (isset($_SESSION['info'])) {
 			<input class='champs_posts' type='text' name='message' placeholder='  Votre message'><br/>
 			<input class='img_publi' type='file' id='img_publi' name='img_publi'><br/>
 			<input class='valider_posts' type='submit' value='Publier'>
-			</form></div>";
+			</form></div> </div>";
             
         }/* else {
             echo /*"<li><a href='index.php?action=login'>Login</a></li>"
