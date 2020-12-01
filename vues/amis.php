0a82a3f9ff820b4533ec8620a86f70694a3fb06f
DeplanqueMathis <deplanque.mathis@gmail.com>
2020-12-01 15:04:24 +0100

    Affichage si pas d'amis/invitations

--- vues/amis.php
+++ vues/amis.php
@@ -3,11 +3,16 @@
 $sql = "SELECT * FROM user WHERE id IN ( SELECT user.id FROM user INNER JOIN lien ON idUtilisateur1=user.id AND etat='ami' AND idUTilisateur2=? UNION SELECT user.id FROM user INNER JOIN lien ON idUtilisateur2=user.id AND etat='ami' AND idUTilisateur1=?)";
 $q = $pdo->prepare($sql);
 $q->execute(array($_SESSION['id'],$_SESSION['id']));
-while($line=$q->fetch()) {
-  echo "<nav><div style='border : 1px solid black; width : 10rem'>";
-  echo "<a href='index.php?id=" . $line["id"] . "'>" . $line['login'] . "</a><br/>";
-  echo "</div></nav>";
+$results = $q->fetchall();
+if(empty($results)){
+	echo "<nav>Vous n'avez pas d'amis prenez un Curly !</nav>";
 }
-
+else{
+	foreach($results as $line) {
+		echo "<nav><div style='border : 1px solid black; width : 10rem'>";
+		echo "<a href='index.php?id=" . $line["id"] . "'>" . $line['login'] . "</a><br/>";
+		echo "</div></nav>";
+	}
+}
 
 ?>
--- vues/invitrecues.php
+++ vues/invitrecues.php
@@ -3,17 +3,19 @@ $sql = "SELECT user.* FROM user WHERE id IN(SELECT idUtilisateur1 FROM lien WHER
 
 $q = $pdo->prepare($sql);
 $q->execute(array($_SESSION['id']));
-while($line=$q->fetch()) {
-	echo "<nav><div style='border : 1px solid black; width : 10rem'>";
-	echo "<a href='index.php?id=" . $line['id'] . "'>" . $line['login'] . "</a><br/>";
-	echo "<form action='index.php?action=accept' method='POST'>";
-    echo "<input type='number' name='id' value='" . $line['id'] . "' style='display : none;'>";
-    echo "<input type='submit' value='Accepter'>";
-    echo "</form>";
-	echo "En attente d'une réponse";
-	echo "</div></nav>";
-}
-if($line!=$q->fetch()){
-  echo "Vous n'avez aucune demande en attente";
-}
+  if(empty($results)){
+		echo "<nav> Vous n'avez aucune demande en attente</nav>";
+	}
+	else{
+		foreach($results as $line){
+			echo "<nav><div style='border : 1px solid black; width : 10rem'>";
+			echo "<a href='index.php?id=" . $line['id'] . "'>" . $line['login'] . "</a><br/>";
+			echo "<form action='index.php?action=accept' method='POST'>";
+			echo "<input type='number' name='id' value='" . $line['id'] . "' style='display : none;'>";
+			echo "<input type='submit' value='Accepter'>";
+			echo "</form>";
+			echo "En attente d'une réponse";
+			echo "</div></nav>";
+		}
+	}
 ?>
