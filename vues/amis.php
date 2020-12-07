534b92924f67cee0d4314215e44dea4ca541bc28
DeplanqueMathis <deplanque.mathis@gmail.com>
2020-12-07 10:07:12 +0100

    Correction amis

--- vues/amis.php
+++ vues/amis.php
@@ -3,23 +3,9 @@
 $sql = "SELECT * FROM user WHERE id IN ( SELECT user.id FROM user INNER JOIN lien ON idUtilisateur1=user.id AND etat='ami' AND idUTilisateur2=? UNION SELECT user.id FROM user INNER JOIN lien ON idUtilisateur2=user.id AND etat='ami' AND idUTilisateur1=?)";
 $q = $pdo->prepare($sql);
 $q->execute(array($_SESSION['id'],$_SESSION['id']));
-<<<<<<< HEAD
-
-while($line=$q->fetch()) {
-  echo "<nav><div class='mes_amis'>";
-    echo "<p>Ma liste d'amis :</p><br/>";
-  echo "<a href='index.php?id=" . $line["id"] . "'> " . $line['login'] . "<span>Cliquez pour voir son mur</span></a><br/>";
-  echo "</div></nav>";
-
 $results = $q->fetchall();
 if(empty($results)){
 	echo "<nav>Vous n'avez pas d'amis prenez un Curly !</nav>";
-
-=======
-$results = $q->fetchall();
-if(empty($results)){
-	echo "<nav>Vous n'avez pas d'amis prenez un Curly !</nav>";
->>>>>>> e49253eb75ee273fc23ba18fdee12bea1c496f57
 }
 else{
 	echo "<nav><div class='mes_amis'>";
