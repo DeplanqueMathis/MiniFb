252ed8e9362b1e6f9698b00a78e99aaab1c50e76
Francesca <francesca.fdv28@gmail.com>
2020-12-10 10:37:16 +0100

    modif des mentions j'aime

--- vues/accueil.php
+++ vues/accueil.php
@@ -83,7 +83,7 @@
 			echo "<img src='images/img_publi/" . $line['image'] . "'> ";
 		}  /*besoin d'une condition pour dire qu'il n'y pas dimage sinon ca fait image cassé*/
 		echo "Par : <a href='index.php?id=" . $line['idAuteur'] . "'>" . $line['login'] . "</a>";
-		echo "<a href='index.php?action=jaime&id=" . $_SESSION['id'] . "&post=" . $line['0'] . "'>J'aime</a> (" . $line['aime'] . ")";
+		echo "<a class='mention_aime' href='index.php?action=jaime&id=" . $_SESSION['id'] . "&post=" . $line['0'] . "'><img   src='images/aime.png' alt='mention_jaime'></a> (J'aime : " . $line['aime'] . ")";
 		echo "</div> </nav>";
     }
 	echo "</div>";
